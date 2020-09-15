fcbcd62d442b51c59bf8eaa8280dfc7499ecbe34
Ruben Norori <kachomanic@hotmail.com>
2020-09-15 16:42:36 -0600

    crud completed

--- src/routes/links.js
+++ src/routes/links.js
@@ -15,7 +15,36 @@ router.post('/add', async (req, res) => {
         description
     };
     await pool.query('INSERT INTO links set ?', [newLink]);
-    res.send('Received');
+    res.redirect('/links');
+});
+
+router.get('/', async (req, res) => {
+    const links = await pool.query('SELECT * FROM links ORDER BY id DESC');
+    res.render('links/list', {links});
+});
+
+router.get('/delete/:id', async (req, res) => {
+    const { id } = req.params;
+    await pool.query('DELETE FROM links WHERE id = ?', [id]);
+    res.redirect('/links');
+});
+
+router.get('/edit/:id', async (req, res) => {
+    const { id } = req.params;
+    const links = await pool.query('SELECT * FROM links WHERE id = ?', [id]);
+    res.render('links/edit', {link: links[0]});    
+});
+
+router.post('/edit/:id', async (req, res) => {
+    const { id } = req.params;
+    const { title, description, url } = req.body;
+    const newLink = {
+        title,
+        description,
+        url
+    };
+    await pool.query('UPDATE links SET ? WHERE id = ?', [newLink, id]);
+    res.redirect('/links');
 });
 
 
